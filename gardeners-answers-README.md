4f317c7fa522d560d08c03170f0403267fce1d75
lauragreen <lollymay@gmail.com>
2017-11-22 16:16:38 +0000

    First commit

---
+++ gardeners-answers-README.md
@@ -0,0 +1,18 @@
+Using create-react-app and firebase to present the answers given in the radio
+programme "Gardeners' Question Time", by searching topic, season, or specific
+episode. A companion to the sparse 'Factsheet'.
+
+Firebase and create-react-app - article
+https://www.codementor.io/yurio/all-you-need-is-react-firebase-4v7g9p4kf
+
+* remade gardeners-answers in my Github folder with `create-react-app`
+* connected to existing `gardeners-answers` in Firebase
+* Firebase link:
+  https://console.firebase.google.com/u/0/project/gardeners-answers/overview
+* App link: https://gardeners-answers.firebaseapp.com/
+* to edit and deploy:
+  * make a change
+  * build it with `npm run build`
+  * upload new version with `firebase deploy`
+
+Updating json
---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import fire from './fire';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    }; // <- set up react state
+  }
+  componentWillMount() {
+    /* Create reference to data in Firebase Database */
+    let dataRef = fire
+      .database()
+      .ref('data')
+      .orderByKey()
+      .startAt('0')
+      .limitToLast(100);
+    dataRef.on('child_added', this.dataReadFromName);
+  }
+
+  dataReadFromName = snapshot => {
+    /* Update React state when gardener is added at Firebase Database */
+    let entry = {
+      content: snapshot.val(),
+      id: snapshot.key,
+    };
+    this.setState({
+      data: [entry].concat(this.state.data),
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>Show a list of questions and answers</h3>
+        <table>
+          {/* Render the list of gardeners */
+          this.state.data.map(entry => (
+            <tr>
+              <td>
+                {entry.content.question ? 'Question' : ''}{' '}
+                {entry.content.Bob ? 'Bob' : ''}
+                {entry.content.James ? 'James' : ''}
+                {entry.content.Bunny ? 'Bunny' : ''}
+              </td>
+              <td key={entry.id}>{entry.content.text}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
