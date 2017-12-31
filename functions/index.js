9a167d710d6c3df6282385d6df684d5e5bc2f4ea
lauragreen <lollymay@gmail.com>
2017-12-31 10:16:20 +0000

    Revert ":sparkles: begin firebase functions"
    This reverts commit deafd8dab30c7cb86b25df833aed17d5f57a74f9.

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,3 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
-
-// Initialize Algolia, requires installing Algolia dependencies:
-// https://www.algolia.com/doc/api-client/javascript/getting-started/#install
-//
-// App ID and API Key are stored in functions config variables
-const ALGOLIA_ID = functions.config().algolia.app_id;
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
-const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_key;
-
-const ALGOLIA_INDEX_NAME = 'answers';
-const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
