cf9dc90aaf8af083edfccc5af3a551b70d1912fe
lauragreen <lollymay@gmail.com>
2017-12-31 10:16:22 +0000

    :package: re-add firebase functions packages

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,12 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+const algoliasearch = require('algoliasearch');
+const algoliaFunctions = require('algolia-firebase-functions');
+ 
+const algolia = algoliasearch(functions.config().algolia.app,
+                              functions.config().algolia.key);
+ const index = algolia.initIndex(functions.config().algolia.index);
+ 
+ exports.syncAlgoliaWithFirebase = functions.database.ref('/answers').onWrite(
+    event => algoliaFunctions.syncAlgoliaWithFirebase(index, event));
