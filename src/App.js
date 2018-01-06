bed4b9c07649d3440d9dee5d4c2d3cb408f076f0
lauragreen <lollymay@gmail.com>
2018-01-06 08:01:35 +0000

    :recycle: coding react-instantsearch correctly

--- src/App.js
+++ src/App.js
@@ -16,28 +16,13 @@ function Search() {
 function Item({hit}) {
   return (
     <div className="hit-name">
-      <div>
         <p>
           Q <Highlight attributeName="question" hit={hit} />
         </p>
+        <div className="answers">
+          <Highlight attributeName="answers" separator={<br />} hit={hit} />
         </div>
-        <div>
-        <ul>
-          <li>
-            Bob's answer: <Highlight attributeName="answers.Bob" hit={hit} />
-            <br />
-          </li>
-          <li>
-            Pippa's answer: <Highlight attributeName="answers.Pippa" hit={hit} />
-            <br />
-          </li>
-          <li>
-            Chris' answer: <Highlight attributeName="answers.Chris" hit={hit} />
-            <br />
-          </li>
-        </ul>
-      </div>
-    </div>
+  </div>
   );
 };
 
@@ -45,18 +30,63 @@ const Hit = ({hit}) =>
   <div className="hit">
   </div>
 
-const App = () => {
-  return (
-    <div>
-      <h1>Gardeners’ Answers</h1>
-      <div className="mainContainer">
-        <InstantSearch appId="YNWU87GBPU" apiKey="a43d81159b0ffc8eaf3812af985f5262" indexName="answers">
-          <Search />
-        </InstantSearch>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    }; // <- set up react state
+  }
+  componentWillMount() {
+    /* Create reference to data in Firebase Database */
+    let dataRef = fire
+      .database()
+      .ref('data')
+      .orderByKey()
+      .limitToLast(100);
+    dataRef.on('child_added', this.dataReadFromName);
+  }
+
+  dataReadFromName = snapshot => {
+    /* Update React state when gardener is added at Firebase Database */
+    let entry = {
+      content: snapshot.val(),
+      id: snapshot.key,
+    };
+    this.setState({
+      data: [entry].concat(this.state.data),
+    });
+  };
+
+  render() {
+    return <div>
+    <h1>Gardeners’ Answers</h1>
+    <div className="mainContainer">
+      <InstantSearch appId="YNWU87GBPU" apiKey="a43d81159b0ffc8eaf3812af985f5262" indexName="answers">
+        <Search />
+      </InstantSearch>
+      <hr />
+
+    
+    {this.state.data.map(entry => <div key={entry.id}>
+      <div>
+        <p>Q {entry.content.question}</p>
+      </div>
+      <div>
+        <ul>
+          {Object.entries(entry.content.answers).map(answer => (
+            <li key={answer[0]}>
+              <span className="panellistName">{answer[0]}</span>{' '}
+              &mdash; {answer[1]}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-)
+    )}
+    </div>
+  </div>;
+  }
 }
-
 
 export default App;
